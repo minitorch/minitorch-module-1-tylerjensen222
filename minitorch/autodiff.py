4ca4be426fd51f5d146824ca5e24a511a0b39b7a
Tyler_Jensen <tyjensen222>
2024-09-02 12:32:55 -0600

    Implement central difference function.

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,15 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    vals = list(vals)
+
+    vals[arg] += epsilon
+    f_plus = f(*vals)
+
+    vals[arg] -= 2 * epsilon
+    f_minus = f(*vals)
+
+    return (f_plus - f_minus) / (2 * epsilon)
 
 
 variable_count = 1
